7970fc2749ec6834fb695cb91d4aa3ee2ffe23b6
LC_Linkous <L.C.Linkous@gmail.com>
2025-01-23 21:11:09 -0500

    added .py for detailed library help

---
+++ src/tinySA_help.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python3
+
+##------------------------------------------------------------------------------------------------\
+#   tinySA_python 
+#   './tinySA_python.py'
+#   UNOFFICIAL Python API based on the tinySA Ultra official documentation at https://www.tinysa.org/wiki/
+#   This class has 1 function to return the 'libraryHelp' function text.
+#   Might be moved to a txt file. depends on what's easier to read.   
+#
+#
+#   Author(s): Lauren Linkous
+#   Last update: January 20, 2025
+##--------------------------------------------------------------------------------------------------\
+
+
+
+def libraryHelp():
+
+    return
