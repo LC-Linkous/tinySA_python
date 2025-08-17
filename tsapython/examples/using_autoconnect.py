1f877c96ef698a1a6b37b74fc64e6d9db2f51b06
LC_Linkous <L.C.Linkous@gmail.com>
2025-08-17 16:48:09 -0400

    autoconnect example updated

---
+++ tsapython/examples/using_autoconnect.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python3
+
+##-------------------------------------------------------------------------------\
+#   tinySA_python (tsapython)
+#   './examples/using_autoconnect.py'
+#   This is an example of using the autoconnect feature. 
+#   The detected device ID is returned and the serial disconnected
+#
+#   Last update: August 17, 2025
+##-------------------------------------------------------------------------------\
+
+# import tinySA_python (tsapython) package
+from tsapython import tinySA
+
+# create a new tinySA object    
+tsa = tinySA()
+
+# set the return message preferences 
+tsa.set_verbose(True) #detailed messages
+tsa.set_error_byte_return(True) #get explicit b'ERROR' if error thrown
+
+
+# attempt to autoconnect
+found_bool, connected_bool = tsa.autoconnect()
+
+# if port found and connected, then complete task(s) and disconnect
+if connected_bool == True: 
+    print("device connected")
+
+    msg = tsa.get_device_id() 
+    print(msg)
+    
+
+    tsa.disconnect()
+else:
+    print("ERROR: could not connect to port")
